415aae0b97141506f9da918a2f0add755db3fa75
mpesaran <mahsa.pesaran@gmail.com>
2025-01-06 12:11:39 +1100

    Free memory when the parent is null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,7 +16,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!new_node)
 		return (NULL);
 	if (!parent)
+	{
+		free(new_node);
 		return (NULL);
+	}
 	if (parent->left == NULL)
 	{
 		new_node->parent = parent;
