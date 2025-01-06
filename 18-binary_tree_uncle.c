f9efe9b415ca8a14aafce473610c90f85ce7741b
mpesaran <mahsa.pesaran@gmail.com>
2025-01-06 18:25:08 +1100

    Delete extra whitespaces

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 /**
- * binary_tree_sibling -  finds the sibling of a node
- * @node: pointer to the node to find the sibling
- *
- * Return: pointer to the sibling, otherwise NULL
+ * binary_tree_uncle -  finds the uncle of a node
+ * @node: pointer to the node to find the uncle
+ * 
+ * Return: pointer to the uncle, otherwise NULL
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node)
 		return (NULL);
